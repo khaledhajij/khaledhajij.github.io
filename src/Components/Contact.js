e1d234c0b4866427338daa9228ae8e8cb6789225
khaledhajij <hajij94@gmail.com>
2022-11-27 17:58:44 +0200

    fixing about height

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,7 +4,7 @@ import { sectionsData } from "../data";
 
 const Contact = () => {
   return (
-    <div className='Contact' id>
+    <div className='Contact' id='contact'>
         <div className="myContainer">
             <Section h2={sectionsData.contact.h2} p={sectionsData.contact.p.toLowerCase()} />
         </div>
